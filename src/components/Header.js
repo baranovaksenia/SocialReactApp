eb60fe9ba64be2ec8eec2a6e1edd59c80db908a0
Ksenia Baranova <baranovaksenia1990@gmail.com>
2021-04-19 20:36:13 +0300

    layout

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <a href="#" className="logo">
+        Keter Finance
+      </a>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://image.freepik.com/free-vector/happy-family-couple-with-kids-pet-standing-together_179970-892.jpg"
+          alt=""
+        />
+      </div>
+      <div>Lorem</div>
+      <div>
+        Lorem
+        <div>Loremm</div>
+        <div>
+          <div>Loremm</div>
+          <div>Loremm</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
